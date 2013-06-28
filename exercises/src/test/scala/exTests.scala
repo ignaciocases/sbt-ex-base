9af50821b56c2f66dab81279519b9a4c8b752104
Ignacio Cases <ignacio.cases@gmail.com>
2013-06-28 13:24:55 -0400

    removed unfinished example

--- exercises/src/test/scala/exTests.scala
+++ exercises/src/test/scala/exTests.scala
@@ -11,12 +11,6 @@ class Ex2 extends Specification { def is = s2"""
    start with F(0) = 0                                           $e212
    end with F(20) = 2584                                         $e213
 
- Ex 2.2
- The Fibonacci result should
-   contain 20 elements                                           $e221
-   start with F(0) = 0                                           $e222
-   end with F(20) = 2584                                         $e223
-
                                                                  """
 
   import Ex2_1._
@@ -25,9 +19,5 @@ class Ex2 extends Specification { def is = s2"""
   def e212 = twentyFibos.head === (1)
   def e213 = twentyFibos.last === (2584)
 
-
-  def e221 = twentyFibos must have size(20)
-  def e222 = twentyFibos.head === (1)
-  def e223 = twentyFibos.last === (2584)
 
 }
